da2960e350742e29b8d0d702a4701d5e6f78876b
lgristina <79229566+lgristina@users.noreply.github.com>
2023-09-24 23:03:48 +0000

    comments and name changes

--- Lab 2/lab2.erl
+++ Lab 2/lab2.erl
@@ -4,18 +4,19 @@
 % -- Public --
 %
 
--export([func/2, formatLists/1]).
+-export([generateLists/2, formatLists/1]).
 
 
 % Debugged and slightly altared by chatgpt
 % The inner list comprehension generates the N-length sequences spaced by M.
 % The outer list comprehension generates the M lists.
 
-func(N, M) -> [ [X + (M*Y) || Y <- lists:seq(0, N-1)] || X <- lists:seq(M, 1, -1) ].
+generateLists(N, M) -> [ [X + (M*Y) || Y <- lists:seq(0, N-1)] || X <- lists:seq(M, 1, -1) ].
 
 
 % Formats the list to avoid the pattern matching of ascii values in the lists.
-formatLists([]) -> ok;
+
+formatLists([]) -> ok; % prints ok instead of an empty list
 formatLists([HeadList | TailLists]) -> io:format("~w~n", [HeadList]), formatLists(TailLists).
 
 % -- Private --
