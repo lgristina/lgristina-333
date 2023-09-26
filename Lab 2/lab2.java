c617760d81ed20451d4641826b23e8e86c1c6e05
lgristina <79229566+lgristina@users.noreply.github.com>
2023-09-26 19:21:45 +0000

    added input handling and more test cases in java file

--- Lab 2/lab2.java
+++ Lab 2/lab2.java
@@ -3,9 +3,13 @@ import java.util.List;
 
 public class lab2{
 
-    // creates the lists using the 2 values taken in and returns a list of the lists
+    // creates the lists using the 2 values taken in and returns the list of the lists
     public static List<List<Integer>> generateLists(int length, int spacer){
 
+        if(!(length >= 0 && length == (int)length) || !(spacer > 0 && spacer == (int)spacer)){
+            throw new IllegalArgumentException("Error: Please input a non-negative integer");
+        }
+        
         List<List<Integer>> lists = new ArrayList<>();
 
         // calculates and populates the values in each list
@@ -22,22 +26,84 @@ public class lab2{
     
     public static void main(String[] args){
         
-        // Test case for the example on the lab page
-        List<List<Integer>> lists = generateLists(6, 14);
+        System.out.println();
+        System.out.println("-----------------------------------------");
+        System.out.println("Test Case 1: example from lab page");
+        System.out.println();
 
-        for(List<Integer> list : lists){
-            System.out.println(list);
+        try {
+            
+            // Test case for the example on the lab page
+            List<List<Integer>> lists = generateLists(6, 14);
+
+            for(List<Integer> list : lists){
+                System.out.println(list);
+            }
+        } catch (IllegalArgumentException exception) {
+            
+            System.out.println(exception.getMessage());
         }
         
         System.out.println();
         System.out.println("-----------------------------------------");
+        System.out.println("Test Case 2: More expected values");
         System.out.println();
 
-        // Another test case with expected values
-        List<List<Integer>> lists2 = generateLists(5, 10);
+        try{
+            
+            // Another test case with expected values
+            List<List<Integer>> lists2 = generateLists(5, 10);
 
-        for(List<Integer> list : lists2){
-            System.out.println(list);
+            for(List<Integer> list : lists2){
+                System.out.println(list);
+            }
         }
+        catch(IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
+
+        System.out.println();
+        System.out.println("-----------------------------------------");
+        System.out.println("Test Case 3: negative integer handling");
+        System.out.println();
+ 
+        try{
+            
+            // Test case for negative integers
+            List<List<Integer>> lists3 = generateLists(-5,6);
+
+            for(List<Integer> list : lists3){
+                System.out.println(list);
+            }
+        }
+        catch(IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
+
+        System.out.println();
+        System.out.println("-----------------------------------------");
+        System.out.println("Test Case 4: Wanted to use a character to test my handling"); 
+        System.out.println("but it wouldn't compile bc its not an integer obviously"); 
+        System.out.println();
+
+        try{
+            
+            /* 
+            Wanted to test for more unexpected inputs (ie. char / String) but obviously it wouldn't 
+            compile because the generateLists method takes in only ints as its parameters
+            */ 
+            List<List<Integer>> lists4 = generateLists(-7, 8);
+
+            for(List<Integer> list : lists4){
+                System.out.println(list);
+            }
+        }
+        catch(IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
+
+        System.out.println();
+        System.out.println("-----------------------------------------");
+        System.out.println();
     }
 }
